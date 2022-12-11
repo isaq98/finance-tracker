458dbe66565e15be3099c153d716d7756c917e47
Sameer Isaq <shisaq@dons.usfca.edu>
2022-12-11 14:55:54 -0800

    Adding a landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PrimaryTable from 'Components/PrimaryTable/PrimaryTable';
 import ExpenseForm from 'Components/ExpenseForm/ExpenseForm';
+import LandingPage from 'Components/LandingPage';
 
 function App() {
   return (
     <div>
-      <ExpenseForm/>
-      <PrimaryTable className='main-menu-table' headers={['Categories', 'Cost', 'Date', 'Description']}/>
+      <LandingPage/>
     </div>
   )
 }
---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './_LandingPage.scss';
+
+function LandingPage() {
+    return (
+        <div className="landing-page">
+            <div className="center-content">
+                <div className="landing-text">
+                    <p>Good afternoon, what would you like to do?</p>
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
