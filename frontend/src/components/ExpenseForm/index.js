906cf9fa6e1097845350c9d30f1204f4fc29a51d
Sameer Isaq <shisaq@dons.usfca.edu>
2022-12-17 23:35:56 -0800

    Sprucing up the ExpenseForm component

--- frontend/src/components/ExpenseForm/index.js
+++ frontend/src/components/ExpenseForm/index.js
@@ -14,22 +14,24 @@ function ExpenseForm(props) {
     }
     const {className} = props;
     return (
-        <div className={`${className ? className : ''}input-form`}>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="category">Category: </label>
-                <select id="category" name="category">
-                    <option value="constant">Constants</option>
-                    <option value="food">Food</option>
-                    <option value="miscellaneous">Miscellaneous</option>
-                </select>
-                <label htmlFor="cost">Cost: </label>
-                <input type="text" id="cost" name="cost" value={cost} onChange={event => setCost(event.target.value)}></input>
-                <label htmlFor="date">Date: </label>
-                <input type="date" id="date" name="date"></input>
-                <label htmlFor="description">Description: </label>
-                <input type="text" id="description" name="description" value={description} onChange={event => setDescription(event.target.value)}></input>
-                <button type="submit">Create</button>
-            </form>
+        <div className="container">
+            <div className={`${className ? className : ''}input-form`}>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="category">Category: </label>
+                    <select id="category" name="category">
+                        <option value="constant">Constants</option>
+                        <option value="food">Food</option>
+                        <option value="miscellaneous">Miscellaneous</option>
+                    </select>
+                    <label htmlFor="cost">Cost: </label>
+                    <input type="text" id="cost" name="cost" value={cost} onChange={event => setCost(event.target.value)}></input>
+                    <label htmlFor="date">Date: </label>
+                    <input type="date" id="date" name="date"></input>
+                    <label htmlFor="description">Description: </label>
+                    <input type="text" id="description" name="description" value={description} onChange={event => setDescription(event.target.value)}></input>
+                    <button type="submit">Create</button>
+                </form>
+            </div>
         </div>
     )
 }
