3e9d21b09f5f6d931ecf91002cae4a6f1c2fe9aa
Sameer Isaq <shisaq@dons.usfca.edu>
2023-06-22 23:43:58 -0700

    Adjusting styling for submitting an expense

--- frontend/src/components/ExpenseForm/index.js
+++ frontend/src/components/ExpenseForm/index.js
@@ -41,20 +41,16 @@ function ExpenseForm(props) {
 
     const {className} = props;
     return (
-        <div className="container">
+        <div className="expense-container">
             <div className={`${className ? className : ''}input-form`}>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="category">Category: </label>
                     <select id="category" name="category" onChange={event => setCategory(event.target.value)}>
                         <option value="constant">Constants</option>
                         <option value="food">Food</option>
                         <option value="miscellaneous">Miscellaneous</option>
                     </select>
-                    <label htmlFor="cost"></label>
                     <input type="text" id="cost" name="cost" placeholder="Cost" value={cost} onChange={event => setCost(event.target.value)}></input>
-                    <label htmlFor="date">Date: </label>
                     <input type="date" id="date" name="date" onChange={event => setDate(event.target.value)}></input>
-                    <label htmlFor="description"></label>
                     <input type="text" id="description" name="description" placeholder="Description" value={description} onChange={event => setDescription(event.target.value)}></input>
                     <FancyButton buttonText='create' propClass='landing' buttonType="submit"/>
                 </form>
